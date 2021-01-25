1d72ba65c9ca8814b6fc0a8b0fcc3461ef929892
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-25 20:05:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,7 +21,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    artist = @@all.detect {|artist| artist.name == name}
+    artist = @@all.detect {|a| a.name == name}
     return artist.nil? ? Artist.new(name) : artist
   end
 
