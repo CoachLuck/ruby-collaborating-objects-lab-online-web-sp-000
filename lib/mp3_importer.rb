520c1959bf40dab72c4f17bb8d5bdc2d97889db6
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-25 19:53:44 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,3 +1,17 @@
 class MP3Importer
+  attr_accessor :path
 
+  def initialize(path)
+    # get all .mp3 files and add to @files
+    @path = path
+  end
+
+  def files
+    # load all mp3 files and remove path from the name
+
+  end
+
+  def import
+    # create songs from a file name and import into the library
+  end
 end
