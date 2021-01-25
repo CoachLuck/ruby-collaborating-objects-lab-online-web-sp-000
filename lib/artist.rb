836067cc7af3218b3a48cd52a5c18e086fb7de0d
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-25 19:42:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,23 @@
 class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def songs
+    Song.all.select {|song| song.artist = self }
+  end
+
+  def find_or_create_by_name(name)
+    add_song(Song.new(name)) if !Song.all.include?(name)
+  end
 
 end
