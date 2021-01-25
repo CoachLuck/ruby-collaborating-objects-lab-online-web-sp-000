ea6e734ffde2d74efd6cc561102a6ffad6a696f3
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-25 20:32:47 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -11,7 +11,7 @@ class MP3Importer
     names = Dir["#{path}/*.mp3"]
     @size = names.length
     names = names.collect {|name| name[@path.length + 1, name.length]}
-    puts "#{names}"
+    @files = names
   end
 
   def import
