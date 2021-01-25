a2f954e63cf35cdff9347a420903314616e25643
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-25 20:35:53 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,6 +1,6 @@
 class MP3Importer
   attr_accessor :path
-
+  @@files = []
   def initialize(path)
     # get all .mp3 files and add to @files
     @path = path
@@ -10,11 +10,11 @@ class MP3Importer
   def files
     # load all mp3 files and remove path from the name
     names = Dir["#{path}/*.mp3"]
-    @files = names.collect {|name| name[@path.length + 1, name.length]}
+    @@files << names.collect {|name| name[@path.length + 1, name.length]}
   end
 
   def import
-    @files.each {|name| Song.new_by_filename(name)}
+    @@files.flatten.each {|name| Song.new_by_filename(name)}
     # create songs from a file name and import into the library
   end
 end
